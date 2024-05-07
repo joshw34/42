315c9a4df7ed54e28dd80abd6ff34a4b83c84439
joshw34 <159394665+joshw34@users.noreply.github.com>
2024-05-07 18:14:23 +0200

    (null)
    accounted for null strings and pointers

--- ft_printf/print_ptr.c
+++ ft_printf/print_ptr.c
@@ -5,6 +5,11 @@ void	ft_print_ptr(void *ptr, size_t *char_count)
 	unsigned long	addr;
 
 	addr = (unsigned long) ptr;
+	if (addr == 0)
+	{
+		ft_print_str("(nil)", char_count);
+		return;
+	}
 	ft_print_str("0x", char_count);
 	ft_print_hex_lower(addr, char_count);
 }
--- ft_printf/print_text.c
+++ ft_printf/print_text.c
@@ -11,6 +11,11 @@ void	ft_print_str(char *str, size_t *char_count)
 	size_t	len;
 	size_t	i;
 
+	if (str == NULL)
+	{
+		ft_print_str("(null)", char_count);
+		return;
+	}
 	len = ft_strlen(str);
 	i = 0;
 	while (i < len)
