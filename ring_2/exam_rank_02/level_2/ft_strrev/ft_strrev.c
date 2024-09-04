b9d7cb697cc38ff846605c6cd082de732212d019
joshw34 <159394665+joshw34@users.noreply.github.com>
2024-09-04 23:03:19 +0200

    Exam 02 Prep

--- ring_2/exam_rank_02/level_2/ft_strrev/ft_strrev.c
+++ ring_2/exam_rank_02/level_2/ft_strrev/ft_strrev.c
@@ -6,46 +6,7 @@
 /*   By: jwhitley <jwhitley@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 15:45:01 by jwhitley          #+#    #+#             */
-/*   Updated: 2024/08/16 16:11:47 by jwhitley         ###   ########.fr       */
+/*   Updated: 2024/09/04 22:58:41 by jwhitley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strrev(char *str)
-{
-	int		i;
-	int		j;
-	char	*temp;
-
-	if (!str)
-		return (NULL);
-	i = 0;
-	j = ft_strlen(str) - 1;
-	temp = str;
-	while (j >= 0)
-	{
-		str[j] = temp[i];
-		i++;
-		j--;
-	}
-	return (str);
-}
-
-int	main(int argc, char **argv)
-{
-	(void)argc;
-	printf("Original:  %s\n", argv[1]);
-	printf("ft_strrev: %s\n", ft_strrev(argv[1]));
-	return (0);
-}
--- ring_2/exam_rank_02/level_2/ft_strspn/ft_strspn.c
+++ ring_2/exam_rank_02/level_2/ft_strspn/ft_strspn.c
@@ -6,12 +6,12 @@
 /*   By: jwhitley <jwhitley@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 14:38:53 by jwhitley          #+#    #+#             */
-/*   Updated: 2024/08/22 14:53:09 by jwhitley         ###   ########.fr       */
+/*   Updated: 2024/09/04 22:40:20 by jwhitley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-//#include <string.h>
+#include <stdio.h>
+#include <string.h>
 #include <stddef.h>
 
 static	int	comp_chars(const char c, const char *accept)
@@ -42,11 +42,11 @@ size_t	ft_strspn(const char *s, const char *accept)
 	return (i);
 }
 
-/*int	main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	(void)argc;
 	printf("String: %s\tAccept: %s\n", argv[1], argv[2]);
 	printf("ft_strspn: %ld\n", ft_strspn(argv[1], argv[2]));
 	printf("lc_strspn: %ld\n", strspn(argv[1], argv[2]));
 	return (0);
-}*/
+}
