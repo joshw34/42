aec6af0af774d488efd68799c7dc9324a5ba6e54
joshw34 <159394665+joshw34@users.noreply.github.com>
2024-09-18 11:11:21 +0200

    Created philo Makefile

---
+++ ring_3/philosophers/inc/philo.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwhitley <jwhitley@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 10:52:09 by jwhitley          #+#    #+#             */
+/*   Updated: 2024/09/18 10:55:27 by jwhitley         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+
+#endif
