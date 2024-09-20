c5f584cf0a4d25f0e3412d6b793a4f6453a98023
joshw34 <159394665+joshw34@users.noreply.github.com>
2024-09-20 10:57:57 +0200

    Philosophers

--- ring_3/philosophers/src/main.c
+++ ring_3/philosophers/src/main.c
@@ -6,7 +6,7 @@
 /*   By: jwhitley <jwhitley@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 10:51:37 by jwhitley          #+#    #+#             */
-/*   Updated: 2024/09/18 14:41:25 by jwhitley         ###   ########.fr       */
+/*   Updated: 2024/09/19 10:16:28 by jwhitley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@ int	main(int argc, char **argv)
 {
 	t_input	*input;
 
-	input = parse_input(argv);
+	input = parse_input(argv + 1);
 	if (!input)
 		return (1);
 	else
-		printf("Good\n");
-
+		printf("n_philo: %d\nt_die: %d\nt_eat: %d\nt_sleep: %d\nn_eat: %d\n", input->n_philo, input->t_die, input->t_eat, input->t_sleep, input->n_eat);
 	(void)argc;
 }
--- ring_3/philosophers/src/parse_check_input.c
+++ ring_3/philosophers/src/parse_check_input.c
@@ -6,19 +6,50 @@
 /*   By: jwhitley <jwhitley@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 12:05:48 by jwhitley          #+#    #+#             */
-/*   Updated: 2024/09/18 14:40:43 by jwhitley         ###   ########.fr       */
+/*   Updated: 2024/09/19 11:05:43 by jwhitley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	result;
+	
+	i = 0;
+	result = 0;
+	while (str[i])
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			result = (result * 10) + (str[i] - 48);
+		else
+			return (0);
+		i++;
+	}
+	return (result);
+}
+
 t_input	*parse_input(char **argv)
 {
 	t_input	*input;
-	(void)argv;
 
 	input = malloc(sizeof(t_input));
 	if(!input)
 		return (NULL);
-	return (input);		
+	input->n_philo = ft_atoi(argv[0]);
+	input->t_die = ft_atoi(argv[1]);
+	input->t_eat = ft_atoi(argv[2]);
+	input->t_sleep = ft_atoi(argv[3]);
+	if (argv[4])
+		input->n_eat = ft_atoi(argv[4]);
+	else
+		input->n_eat = -1;
+	if (input->n_philo == 0 || input->t_die == 0 || input->t_eat == 0
+		|| input->t_sleep == 0 || input->n_eat == 0)
+	{
+		input_error_exit(input, "Error: Invalid Arguments\n");
+		return (NULL);
+	}
+	return (input);
 }
