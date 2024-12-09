fa1dd195ab2dd760d09934ed0aeac66202da5e25
joshw34 <159394665+joshw34@users.noreply.github.com>
2024-12-09 14:20:47 +0100

    continued

--- ring_3/minishell2/src/check_builtins.c
+++ ring_3/minishell2/src/check_builtins.c
@@ -6,7 +6,7 @@
 /*   By: jwhitley <jwhitley@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 15:33:25 by jwhitley          #+#    #+#             */
-/*   Updated: 2024/12/04 15:33:26 by jwhitley         ###   ########.fr       */
+/*   Updated: 2024/12/09 14:00:09 by jwhitley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ bool	check_builtins(t_data *data)
 		print_pwd(data);
 	else if (ft_strncmp(data->tokens->word, "exit", 5) == 0)
 		exit_cmd(data);
+	else if (ft_strncmp(data->tokens->word, "env", 4) == 0)
+		print_env(data);
 	else if (data->tokens->next == NULL)
 		return (false);
 	else if (ft_strncmp(data->tokens->word, "cd", 3) == 0)
 		change_dir(data, data->tokens->next->word);
 	else if (ft_strncmp(data->tokens->word, "export", 7) == 0)
 		export_env(data, data->tokens->next->word);
-	else if (ft_strncmp(data->tokens->word, "env", 4) == 0)
-		print_env(data);
 	else if (ft_strncmp(data->tokens->word, "unset", 6) == 0)
 		remove_var(data, data->tokens->next->word);
 	else if (ft_strncmp(data->tokens->word, "echo", 5) == 0)
--- ring_3/minishell2/src/var_get_print.c
+++ ring_3/minishell2/src/var_get_print.c
@@ -6,7 +6,7 @@
 /*   By: jwhitley <jwhitley@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 10:57:32 by jwhitley          #+#    #+#             */
-/*   Updated: 2024/11/21 16:04:17 by jwhitley         ###   ########.fr       */
+/*   Updated: 2024/12/09 13:59:03 by jwhitley         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,16 @@ void	print_env(t_data *data)
 	int	i;
 
 	i = 0;
+	printf("1\n");
 	if (!data->env)
 		return ;
+	printf("2\n");
 	while (data->env[i])
 	{
 		printf("%s\n", data->env[i]);
 		i++;
 	}
+	printf("3\n");
 }
 
 char	*get_var(char **env, char *var)
